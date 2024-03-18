8f650564da41c9dee3afb688630dc183cdd10fdb
hweeyun <125211654+hweeyun@users.noreply.github.com>
2024-03-18 14:08:35 +0000

    test snowflake connection

--- streamlit.py
+++ streamlit.py
@@ -1,5 +1,5 @@
 # Import python packages
-import streamlit as st
+# import streamlit as st
 # from snowflake.snowpark.context import get_active_session
 import pandas as pd
 import snowflake.connector
@@ -8,12 +8,12 @@ import snowflake.connector
 st.title("Zena Amazing Athleisure Catalog")
 
 # Check connection
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+st.text("Hello from Snowflake:")
+st.text(my_data_row)
 
 st.stop()
 
