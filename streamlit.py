b806f5fa3363c2ee193c3ec025b9c03cfcf8f00c
hweeyun <125211654+hweeyun@users.noreply.github.com>
2024-03-18 13:42:47 +0000

    Create streamlit.py

---
+++ streamlit.py
@@ -0,0 +1,33 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+import pandas as pd
+
+# Write directly to the app
+st.title("Zena Amazing Atheleisure Catalog")
+
+# Get the current credentials
+session = get_active_session()
+
+df = session.table("ZENAS_ATHLEISURE_DB.PRODUCTS.catalog_for_website")
+# st.dataframe(data=df, use_container_width=True)
+
+pd_df = df.to_pandas()
+
+
+color_list = pd_df['COLOR_OR_STYLE']
+
+color_or_style = st.selectbox('Pick a sweatsuit color or style: ', color_list)
+
+image = pd_df[pd_df['COLOR_OR_STYLE']==color_or_style]['DIRECT_URL'].iloc[0]
+st.image(image, caption ='Our warm, comfortable, '+ color_or_style + ' sweatsuit!')
+
+
+price = pd_df[pd_df['COLOR_OR_STYLE']==color_or_style]['PRICE'].iloc[0]
+st.write('Price: ', price)
+
+sizes_avail = pd_df[pd_df['COLOR_OR_STYLE']==color_or_style]['SIZE_LIST'].iloc[0]
+st.write('Sizes Available: ', sizes_avail)
+
+bonus = pd_df[pd_df['COLOR_OR_STYLE']==color_or_style]['UPSELL_PRODUCT_DESC'].iloc[0]
+st.write('', bonus)
