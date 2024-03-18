1b653ea61902191610289428249ccc643e56c124
hweeyun <125211654+hweeyun@users.noreply.github.com>
2024-03-18 14:04:27 +0000

    test connection to snowflake

--- streamlit.py
+++ streamlit.py
@@ -7,6 +7,15 @@ import snowflake.connector
 # Write directly to the app
 st.title("Zena Amazing Athleisure Catalog")
 
+# Check connection
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
+               
+
 st.stop()
 
 # Get the current credentials
